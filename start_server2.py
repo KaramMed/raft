e1ea11fa733825c8eecbbb20ef183592ab20d0a7
KaramMed <mkaram926@gmail.com>
2024-12-01 15:06:53 +0100

    update

---
+++ start_server2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from raft import RaftNode
+import json
+import time
+
+address_book_fname = 'address_book.json'
+
+if __name__ == '__main__':
+    d = {"node1": {"ip": "192.46.237.85", "port": "2380"}, 
+         "node2": {"ip": "172.105.85.119", "port": "2380"}, 
+         "node3": {"ip": "172.104.149.60", "port": "2380"}}
+        
+    with open(address_book_fname, 'w') as outfile:
+        json.dump(d, outfile)
+
+    s0 = RaftNode(address_book_fname, 'node2', 'follower')
+
+    s0.start()
+    
+    # for the attacker node
+    # after 20 seconds, this node will intercept the current index, term, increment term and send vote requests
+    #time.sleep(20)
+    #s0.transition_to_candidate()
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        s0.stop()
